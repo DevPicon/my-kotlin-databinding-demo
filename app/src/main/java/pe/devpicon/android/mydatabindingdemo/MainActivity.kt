fb9da0cd9191f0fb03596ddbaddc805407d128d9
Armando <picondev@gmail.com>
2017-04-28 17:40:08 -0500

    initial commit

---
+++ app/src/main/java/pe/devpicon/android/mydatabindingdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package pe.devpicon.android.mydatabindingdemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
