c0d2ad8adc29f70d923343ca7f1db5663642d1eb
Armando <picondev@gmail.com>
2017-04-28 18:43:29 -0500

    First example and initial configurations. Pay atention with kotlin-apt plugin in the first line of build.gradle

--- app/src/main/java/pe/devpicon/android/mydatabindingdemo/MainActivity.kt
+++ app/src/main/java/pe/devpicon/android/mydatabindingdemo/MainActivity.kt
@@ -1,12 +1,19 @@
 package pe.devpicon.android.mydatabindingdemo
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import pe.devpicon.android.mydatabindingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this@MainActivity, R
+                .layout.activity_main)
+        val product = Product("1", "Arroz", "250.50")
+        binding.product = product
+
+
     }
 }
---
+++ app/src/main/java/pe/devpicon/android/mydatabindingdemo/Product.kt
@@ -0,0 +1,6 @@
+package pe.devpicon.android.mydatabindingdemo
+
+/**
+ * Created by armando on 4/28/17.
+ */
+data class Product(val id: String, val name: String, val actualPrice: String)
